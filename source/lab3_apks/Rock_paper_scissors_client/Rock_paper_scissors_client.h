592d116247e4aa0c85c22661010da55256ac9668
Andrii Kovalchuk <andrii.kovalchuk.ki.2021@lpnu.ua>
2024-11-17 22:00:09 +0200

    task5

---
+++ source/lab3_apks/Rock_paper_scissors_client/Rock_paper_scissors_client.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <string>
+#include <vector>
+#include <cstdlib> // For system()
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+using namespace std;
+
+bool loadGameState(vector<int>& gameState, string& resultText, string& gameInfo, const string& filename = "game_config.json");
+string createJSON(const vector<int>& array);
+bool parseJSON(const string& json, vector<int>& array, string& resultText, string& gameInfo);
+void saveGameState(const vector<int>& gameState, const string& resultText, const string& gameInfo, const string& filename = "game_config.json");
